4e93436368648f1dfc8a92944ef76062cdc4c92f
Loh CK <ckloh.2012@outlook.com>
2018-01-05 17:26:27 +0800

    Shifted search label into placeholder

--- src/SortableTbl.js
+++ src/SortableTbl.js
@@ -108,7 +108,7 @@ class SortableTbl extends React.Component{
 						{ this.props.search && 
 							(
 								<div className="search-box">
-									Search: <input className="search" type="text" name="" value={this.state.filter} placeholder="Filter Result" onChange={this.filter} />
+									<input className="search" type="text" name="" value={this.state.filter} placeholder="Search" onChange={this.filter} />
 								</div>
 							)}
 						{
